e4ac41c03d3419b25cedeac9f2804a70f4a0574f
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-18 22:41:46 +0300

    jquery, branches change color when hovered

--- javascript/main.js
+++ javascript/main.js
@@ -100,6 +100,23 @@ particlesJS.load('particles-js', 'javascript/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+$('img.branches').each(function(){
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
 
 // document.getElementById('branch').onclick = () =>{document.getElementById('branch').style.backgroundColor = '#9954AB'}
 //
