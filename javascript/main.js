3881071305cb8e2dbd6546d94fa3e386c5fdf3ba
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-19 23:56:08 +0300

    fixed problem with branches not disappearing

--- javascript/main.js
+++ javascript/main.js
@@ -10,38 +10,42 @@
 // }
 // changeFirefliesColor()
 
-// document.querySelectorAll('branches').onclick = e => {
-//   e.target.style.display = 'none'
-// }
+document.getElementById('branch').onclick = e => {
+  e.target.style.display = 'none'
+}
 
-// document.getElementById('branch_2').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_3').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_4').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_5').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_6').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_7').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
-// document.getElementById('branch_8').onclick = e => {
-//   e.target.style.display = 'none'
-// }
-//
+document.getElementById('branch_2').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_3').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_4').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_5').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_6').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_7').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_8').onclick = e => {
+  e.target.style.display = 'none'
+}
+
+document.getElementById('branch_9').onclick = e => {
+  e.target.style.display = 'none'
+}
+
 particlesJS.load('particles-js', 'javascript/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
