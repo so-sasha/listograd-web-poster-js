d1b7fed2502a1646c5a18d48fe9087094754f281
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-19 23:34:13 +0300

    different method for changing svg color

--- javascript/main.js
+++ javascript/main.js
@@ -10,58 +10,58 @@
 // }
 // changeFirefliesColor()
 
-document.getElementById('branch').onclick = e => {
-  e.target.style.display = 'none'
-}
+// document.querySelectorAll('branches').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
-document.getElementById('branch_2').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_3').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_4').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_5').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_6').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_7').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_8').onclick = e => {
-  e.target.style.display = 'none'
-}
-
+// document.getElementById('branch_2').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_3').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_4').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_5').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_6').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_7').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_8').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
 particlesJS.load('particles-js', 'javascript/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-$('img.branches').each(function(){
-  var $img = $(this);
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-  $.get(imgURL, function(data) {
-    var $svg = $(data).find('svg');
-    if(typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass+' replaced-svg');
-    }
-    $svg = $svg.removeAttr('xmlns:a');
-    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-    }
-    $img.replaceWith($svg);
-  }, 'xml');
-});
+// $('img.branches').each(function(){
+//   var $img = $(this);
+//   var imgClass = $img.attr('class');
+//   var imgURL = $img.attr('src');
+//   $.get(imgURL, function(data) {
+//     var $svg = $(data).find('svg');
+//     if(typeof imgClass !== 'undefined') {
+//       $svg = $svg.attr('class', imgClass+' replaced-svg');
+//     }
+//     $svg = $svg.removeAttr('xmlns:a');
+//     if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+//       $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+//     }
+//     $img.replaceWith($svg);
+//   }, 'xml');
+// });
 
 
 // document.getElementById('branch').onclick = () =>{document.getElementById('branch').style.backgroundColor = '#9954AB'}
