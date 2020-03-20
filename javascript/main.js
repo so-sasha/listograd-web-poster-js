f58b447cce02367a8939234d583d39a3fb449e78
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-20 15:01:31 +0300

    delete unnecessary

--- javascript/main.js
+++ javascript/main.js
@@ -49,37 +49,3 @@ document.getElementById('branch_9').onclick = e => {
 particlesJS.load('particles-js', 'javascript/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
-
-// $('img.branches').each(function(){
-//   var $img = $(this);
-//   var imgClass = $img.attr('class');
-//   var imgURL = $img.attr('src');
-//   $.get(imgURL, function(data) {
-//     var $svg = $(data).find('svg');
-//     if(typeof imgClass !== 'undefined') {
-//       $svg = $svg.attr('class', imgClass+' replaced-svg');
-//     }
-//     $svg = $svg.removeAttr('xmlns:a');
-//     if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-//       $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-//     }
-//     $img.replaceWith($svg);
-//   }, 'xml');
-// });
-
-
-// document.getElementById('branch').onclick = () =>{document.getElementById('branch').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_2').onclick = () =>{document.getElementById('branch_2').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_3').onclick = () =>{document.getElementById('branch_3').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_4').onclick = () =>{document.getElementById('branch_4').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_5').onclick = () =>{document.getElementById('branch_5').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_6').onclick = () =>{document.getElementById('branch_6').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_7').onclick = () =>{document.getElementById('branch_7').style.backgroundColor = '#9954AB'}
-//
-// document.getElementById('branch_8').onclick = () =>{document.getElementById('branch_8').style.backgroundColor = '#9954AB'}
