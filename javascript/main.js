6a894e4525cd497097457a655f2aaf516bb6e059
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-22 16:07:23 +0300

    rearrange sound

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,9 @@ document.getElementById('branch').onclick = e => {
   e.target.style.display = 'none'
 }
 
-document.getElementById('branch_2').onclick = e => {
-  e.target.style.display = 'none'
-}
+// document.getElementById('branch_2').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
 document.getElementById('branch_3').onclick = e => {
   e.target.style.display = 'none'
@@ -14,9 +14,9 @@ document.getElementById('branch_4').onclick = e => {
   e.target.style.display = 'none'
 }
 
-document.getElementById('branch_5').onclick = e => {
-  e.target.style.display = 'none'
-}
+// document.getElementById('branch_5').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
 document.getElementById('branch_6').onclick = e => {
   e.target.style.display = 'none'
@@ -26,11 +26,11 @@ document.getElementById('branch_7').onclick = e => {
   e.target.style.display = 'none'
 }
 
-document.getElementById('branch_8').onclick = e => {
-  e.target.style.display = 'none'
-}
+// document.getElementById('branch_8').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
-particlesJS.load('particles-js', 'javascript/particles.json', function() {
+particlesJS.load('particles-js', 'javascript/particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
 
@@ -55,7 +55,20 @@ particlesJS.load('particles-js', 'javascript/particles.json', function() {
 // } while (document.getElementById('branch').style.display === 'none')
 
 let soundElement = document.getElementById('audio')
-let elementFromHTML = document.getElementById('branch_8')
-elementFromHTML.onclick = function() {
-  soundElement.play()
+let disappearingBranch_8 = document.getElementById('branch_8')
+disappearingBranch_8.onclick = function() {
+  soundElement.play();
+  disappearingBranch_8.style.display = 'none';
+}
+
+let disappearingBranch_5 = document.getElementById('branch_5')
+disappearingBranch_5.onclick = function() {
+  soundElement.play();
+  disappearingBranch_5.style.display = 'none';
+}
+
+let disappearingBranch_2 = document.getElementById('branch_2')
+disappearingBranch_2.onclick = function() {
+  soundElement.play();
+  disappearingBranch_2.style.display = 'none';
 }
