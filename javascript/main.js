c7e2df8083332526fdc9616403eaf7d9d4ae0e9b
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-23 16:21:43 +0300

    more text

--- javascript/main.js
+++ javascript/main.js
@@ -63,3 +63,70 @@ document.getElementById('text_2').onclick = e => {
   e.target.style.display = 'none';
   document.getElementById('text_3').style.display = 'block';
 }
+
+document.getElementById('text_3').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('text_4').style.display = 'block';
+}
+
+document.getElementById('text_4').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('text_5').style.display = 'block';
+}
+
+document.getElementById('text_5').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('text_6').style.display = 'block';
+}
+
+document.getElementById('text_6').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('text_7').style.display = 'block';
+}
+
+document.getElementById('text_7').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('text').style.display = 'block';
+}
+
+// Сколько создавать карт
+const CARDS_NUMBER = 10
+// Название файлов с картами
+let cards = [
+  'img/1_card.svg',
+  'img/2_card.svg',
+  'img/3_card.svg',
+  'img/4_card.svg',
+  'img/5_card.svg',
+  'img/6_card.svg',
+  'img/7_card.svg',
+  'img/8_card.svg',
+  'img/9_card.svg',
+  'img/10_card.svg',
+  'img/11_card.svg',
+  'img/12_card.svg',
+  'img/13_card.svg'
+]
+// Функция, которая перемешивает массив с названиямии
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+// Применяем функцию перемешивания
+cards = shuffle(cards)
+
+// Находим контейнер для карт
+// Он нам нужен, чтобы в ЦСС все выровнять
+let cardsContainer = document.getElementById('third__cards-container')
+// При помощи цикла создаем карты и вставляем внутрь контейнера
+for (let i = 0; i < CARDS_NUMBER; i++) {
+  let newCard = document.createElement('img')
+  newCard.src = `img/${cards[i]}`
+  newCard.classList.add('card')
+  newCard.onclick = function() {
+
+  }
+  cardsContainer.append(newCard)
+}
