19fa25af5ab8afa8c54f284929bc767353e748bb
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-03-25 22:59:37 +0300

    simplify, add icon and cursor

--- javascript/main.js
+++ javascript/main.js
@@ -1,59 +1,77 @@
-document.getElementById('branch').onclick = e => {
-  e.target.style.display = 'none';
-  document.getElementById('text').style.display = 'block';
-}
+particlesJS.load('particles-js', 'javascript/particles.json')
+
+let hiddenBranches = 1
+let soundElement = document.getElementById('audio')
+
+Array.from(document.querySelectorAll('.branches')).forEach(element => {
+  element.onclick = e => {
+    if (hiddenBranches % 2 === 0) {
+      soundElement.play();
+    }
+    hiddenBranches += 1
+    e.target.style.display = 'none'
+    if (hiddenBranches === 8) {
+      document.getElementById('text').style.display = 'block';
+    }
+  }
+})
+
+// document.getElementById('branch').onclick = e => {
+//   e.target.style.display = 'none';
+//   document.getElementById('text').style.display = 'block';
+// }
 
 // document.getElementById('branch_2').onclick = e => {
 //   e.target.style.display = 'none'
 // }
 
-document.getElementById('branch_3').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_4').onclick = e => {
-  e.target.style.display = 'none'
-}
+// document.getElementById('branch_3').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_4').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
 // document.getElementById('branch_5').onclick = e => {
 //   e.target.style.display = 'none'
 // }
-
-document.getElementById('branch_6').onclick = e => {
-  e.target.style.display = 'none'
-}
-
-document.getElementById('branch_7').onclick = e => {
-  e.target.style.display = 'none'
-}
+//
+// document.getElementById('branch_6').onclick = e => {
+//   e.target.style.display = 'none'
+// }
+//
+// document.getElementById('branch_7').onclick = e => {
+//   e.target.style.display = 'none'
+// }
 
 // document.getElementById('branch_8').onclick = e => {
 //   e.target.style.display = 'none'
 // }
 
-particlesJS.load('particles-js', 'javascript/particles.json', function () {
-  console.log('callback - particles.js config loaded');
-});
+// particlesJS.load('particles-js', 'javascript/particles.json', function () {
+//   console.log('callback - particles.js config loaded');
+// });
 
-let soundElement = document.getElementById('audio')
-let rustlingBranch_8 = document.getElementById('branch_8')
-rustlingBranch_8.onclick = function() {
-  soundElement.play();
-  rustlingBranch_8.style.display = 'none';
-}
-
-let rustlingBranch_5 = document.getElementById('branch_5')
-rustlingBranch_5.onclick = function() {
-  soundElement.play();
-  rustlingBranch_5.style.display = 'none';
-}
-
-let rustlingBranch_2 = document.getElementById('branch_2')
-rustlingBranch_2.onclick = function() {
-  soundElement.play();
-  rustlingBranch_2.style.display = 'none';
-}
-
+// let soundElement = document.getElementById('audio')
+// let rustlingBranch_8 = document.getElementById('branch_8')
+// rustlingBranch_8.onclick = function() {
+//   soundElement.play();
+//   rustlingBranch_8.style.display = 'none';
+// }
+//
+// let rustlingBranch_5 = document.getElementById('branch_5')
+// rustlingBranch_5.onclick = function() {
+//   soundElement.play();
+//   rustlingBranch_5.style.display = 'none';
+// }
+//
+// let rustlingBranch_2 = document.getElementById('branch_2')
+// rustlingBranch_2.onclick = function() {
+//   soundElement.play();
+//   rustlingBranch_2.style.display = 'none';
+// }
+//
 document.getElementById('text').onclick = e => {
   e.target.style.display = 'none';
   document.getElementById('text_2').style.display = 'block';
@@ -87,7 +105,16 @@ document.getElementById('text_6').onclick = e => {
 
 document.getElementById('text_7').onclick = e => {
   e.target.style.display = 'none';
+  document.getElementById('text_8').style.display = 'block';
+}
+
+document.getElementById('text_8').onclick = e => {
+  e.target.style.display = 'none';
   document.getElementById('text').style.display = 'block';
+}
+//
+document.querySelector('body').onclick = function () {
+  document.getElementById('particles-js').style.color = '#79B9F5';
 }
 
 // // Сколько создавать карт
